fd283af7b5144260d5d75eb96576a18574c338bd
yassersouri <yassersouri@gmail.com>
2014-07-19 19:56:08 +0430

    add inital version which builds well with cmake

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	string imgadr;
+
+	if (argc > 1) {
+		imgadr = argv[1];
+	} else {
+		imgadr = "/Users/yasser/sci-repo/opencv/samples/cpp/pic4.png";
+	}
+
+	cv::Mat img = cv::imread(imgadr, 1);
+
+	if (img.empty()) {
+		cout << 'PROB';
+		return 1;
+	}
+
+	cv::imshow("test", img);
+	cv::waitKey(0);
+
+	return 0;
+}
+
