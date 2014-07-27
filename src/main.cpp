f213ad0c97b80a799e5b32b0184dbd950e95e626
yassersouri <yassersouri@gmail.com>
2014-07-28 02:10:57 +0430

    start using ncurses library

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <ncurses.h>
 
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
-	string imgadr;
-
-	if (argc > 1) {
-		imgadr = argv[1];
-	} else {
-		imgadr = "/Users/yasser/sci-repo/opencv/samples/cpp/pic4.png";
-	}
-
-	cout << imgadr << endl;
+	initscr();
+	printw("Salam");
+	refresh();
+	getch();
+	endwin();
 
 	return 0;
 }
