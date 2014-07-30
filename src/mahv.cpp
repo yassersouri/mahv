f799d9399d27e45ae5ad5b5731406e4587a9858b
yassersouri <yassersouri@gmail.com>
2014-07-30 18:33:29 +0430

    writing the for loop

--- src/mahv.cpp
+++ src/mahv.cpp
@@ -39,7 +39,8 @@ cv::SparseMat calculateSparseFillFront(cv::Mat &fillFront) {
 }
 
 void doMahv(cv::Mat orig, cv::Mat mask, cv::Mat &result, int windowSize = 9) {
-	cv::Mat origFloat, origCIE, maskInv, fillFront, image_padded, mask_padded;
+	cv::Mat origFloat, origCIE, maskInv, fillFront, image_padded, mask_padded, mask_max_1, confidence;
+	cv::Mat temp; //FIXME: delete this line
 	result = cv::Mat::zeros(orig.rows, orig.cols, CV_8UC3);
 
 	// calculate inverse of the mask
@@ -57,8 +58,31 @@ void doMahv(cv::Mat orig, cv::Mat mask, cv::Mat &result, int windowSize = 9) {
 	cv::copyMakeBorder(origFloat, image_padded, offset, offset, offset, offset, cv::BORDER_CONSTANT, cv::Scalar(0));
 	cv::copyMakeBorder(mask, mask_padded, offset, offset, offset, offset, cv::BORDER_CONSTANT, cv::Scalar(0));
 
+	//initial confidence
+	mask.convertTo(mask_max_1, CV_8UC1, 1./255, 0);
+	cv::subtract(cv::Scalar(1), mask_max_1, confidence);
 
+	while( true ) {
+		fillFront = calculateFillFront(mask_max_1);
 
-	fillFront = calculateFillFront(mask);
-	cv::SparseMat sparseFillFront = calculateSparseFillFront(fillFront);
+		if (cv::sum(fillFront) == cv::Scalar(0)) {
+			if (cv::sum(mask_max_1) > cv::Scalar(0)) {
+				// if the remaining mask region is 1 pixel thick.
+				fillFront = mask_max_1;
+
+				// just checking it for my self
+				cout << "URGENT CHECK IF THIS IS CORRECT" << endl;
+				cv::imshow("fillFront", fillFront);
+				cv::waitKey(0);
+			} else {
+				break;
+			}
+		}
+
+		// convert image_padded to grayscale and calculate gradients
+		// or calculate gradients in each channel and add them together
+		// which one is faster?
+	}
+
+	//FIXME: copy the non-padded parts of image_padded to result
 }
